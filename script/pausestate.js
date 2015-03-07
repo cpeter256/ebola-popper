4fb94c6941f1b1e35d332553c7316bd7964043a8
lightjelly <icecreamsaleman@gmail.com>
2015-03-06 22:43:12 -0800

    Change pausestate to buttons instead of key
    Not sure if it works

--- script/pausestate.js
+++ script/pausestate.js
@@ -16,12 +16,38 @@ PauseState.prototype.draw = function(canvas, ctx) {
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.globalAlpha = 1;
+
+	var pausemessage = "PAUSED";
+	ctx.save();
+	ctx.fillStyle = "#FF0000";
+	ctx.translate(canvas.width/2, 80);
+	ctx.scale(4, 4);
+	ctx.textAlign = "center";
+	ctx.fillText(message1, 0, 40);
+
+	ctx.restore;
+	ctx.drawImage(sprites["UnpauseButton"], 20, 350);
+	ctx.drawImage(sprites["MenuButton"], canvas.width-276, 350);//gonna need a menu button for this
 };
+
 PauseState.prototype.onkeydown = function(keyCode) {
 	if (keyCode == 27) { //escape
 		this.pop_state(this);
 	} else if (keyCode == 81) { //q
 		this.pop_state(this);
 		this.menu.pop_world();
+	}
+}; //not sure if we need to use key if we have buttons
+
+LevelOverState.prototype.onmousedown = function(e) {
+
+	if(e.button == 0){
+		if(e.layerX>=20 && e.layerX<=20+256 && e.layerY>=350 && e.layerY<=350+96){
+			this.pop_state(this);
+		}
+		if(e.layerX>=this.canvas.width-276 && e.layerX<=this.canvas.width-20 && e.layerY>=350 && e.layerY<=350+96){
+			this.pop_state(this);
+			this.menu.pop_world();
+		}
 	}
 };
